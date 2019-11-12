383f195984be937f3984c3ac008cd37c061a2291
pipiz <zkx6648@gmail.com>
2019-11-12 10:54:55 +0800

    发送数据的逻辑

---
+++ src/constants.rs
@@ -0,0 +1,4 @@
+pub const STAR: u8 = b'*';
+pub const CR: u8 = b'\r';
+pub const LF: u8 = b'\n';
+pub const DOLLAR: u8 = b'$';
--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@ use std::io;
 use std::io::{BufWriter, Write};
 use std::net::{Shutdown, SocketAddr, TcpStream};
 
-use byteorder::{LittleEndian, WriteBytesExt};
-
 use crate::config::Config;
+use crate::constants::*;
 
+mod constants;
 mod config;
 
 pub trait RedisEventListener {
@@ -38,42 +38,23 @@ impl StandaloneEventListener {
     }
 
     fn send(&mut self, command: &[u8], args: &[&[u8]]) {
-        let mut buf: std::vec::Vec<u8> = vec![];
-
-        let writer = self.socket.as_mut().unwrap();
-
-        buf.write_u8(b'*');
-//        socket.write(&[b'*']).unwrap();
+        let socket = self.socket.as_mut().unwrap();
+        socket.write(&[STAR]).unwrap();
         let args_len = args.len() + 1;
-        buf.write_uint::<LittleEndian>(args_len as u64, 8);
-//        socket.write(&args_len).unwrap();
-//        socket.write(&[b'\r', b'\n', b'$']).unwrap();
-        buf.write(&[b'\r', b'\n']);
-        buf.write_u8(b'$');
-
-//        let cmd_len = command.len().to_be_bytes();
-        buf.write_uint::<LittleEndian>(command.len() as u64, 8);
-//        socket.write(&cmd_len).unwrap();
-//        socket.write(&[b'\r', b'\n']).unwrap();
-        buf.write(command).unwrap();
-        buf.write(&[b'\r', b'\n']).unwrap();
+        socket.write(args_len.to_string().as_bytes()).unwrap();
+        socket.write(&[CR, LF, DOLLAR]).unwrap();
+        socket.write(command.len().to_string().as_bytes()).unwrap();
+        socket.write(&[CR, LF]).unwrap();
+        socket.write(command).unwrap();
+        socket.write(&[CR, LF]).unwrap();
         for arg in args {
-            buf.write_u8(b'$');
-//            let arg_len = arg.len().to_be_bytes();
-//            socket.write(&arg_len).unwrap();
-            buf.write_uint::<LittleEndian>(arg.len() as u64, 8);
-            buf.write(&[b'\r', b'\n']).unwrap();
-            buf.write(arg).unwrap();
-            buf.write(&[b'\r', b'\n']).unwrap();
+            socket.write(&[DOLLAR]).unwrap();
+            socket.write(arg.len().to_string().as_bytes()).unwrap();
+            socket.write(&[CR, LF]).unwrap();
+            socket.write(arg);
+            socket.write(&[CR, LF]).unwrap();
         }
-        let x = buf.as_slice();
-        println!("cmd size: {}", x.len());
-        for x in x {
-            print!("{},", x);
-        }
-        println!();
-        writer.write(x);
-        writer.flush();
+        socket.flush();
     }
 }
 
