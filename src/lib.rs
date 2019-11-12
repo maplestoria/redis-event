aad2376e4775d01f98b926a2d8666ad2ec0b8315
pipiz <zkx6648@gmail.com>
2019-11-12 16:24:04 +0800

    获取port

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::io::{BufWriter, Error, ErrorKind, Read, Write};
-use std::net::{Shutdown, TcpStream};
+use std::net::{Shutdown, SocketAddr, TcpStream};
 use std::result::Result;
 use std::result::Result::Ok;
 
@@ -19,33 +19,31 @@ pub trait RedisEventListener {
 
 // 用于监听Redis单点的事件
 pub struct StandaloneEventListener {
-    host: &'static str,
-    port: i32,
+    addr: SocketAddr,
     password: &'static str,
     config: Config,
-    writer: Option<BufWriter<TcpStream>>,
+    stream: Option<TcpStream>,
 }
 
 impl StandaloneEventListener {
     fn connect(&mut self) -> Result<(), Error> {
-        let addr = format!("{}:{}", self.host, self.port);
-        println!("connecting to {}", addr);
-        let stream = TcpStream::connect(addr)?;
+        let stream = TcpStream::connect(self.addr)?;
         println!("connected to server!");
-        self.writer = Option::Some(BufWriter::new(stream));
+        self.stream = Option::Some(stream);
         Ok(())
     }
 
     fn auth(&mut self) -> Result<(), Error> {
         if !self.password.is_empty() {
-            self.send("AUTH".as_bytes(), &[self.password.as_bytes()])?;
+            self.send(b"AUTH", &[self.password.as_bytes()])?;
             self.response()?;
         }
         Ok(())
     }
 
     fn send(&mut self, command: &[u8], args: &[&[u8]]) -> Result<(), Error> {
-        let writer = self.writer.as_mut().unwrap();
+        let stream = self.stream.as_ref().unwrap();
+        let mut writer = BufWriter::new(stream);
         writer.write(&[STAR])?;
         let args_len = args.len() + 1;
         writer.write(args_len.to_string().as_bytes())?;
@@ -65,8 +63,7 @@ impl StandaloneEventListener {
     }
 
     fn response(&mut self) -> Result<Vec<u8>, Error> {
-        let writer = self.writer.as_mut().unwrap();
-        let socket = writer.get_mut();
+        let mut socket = self.stream.as_ref().unwrap();
         let response_type = socket.read_u8()?;
         match response_type {
             PLUS | MINUS => { // Plus: Simple String; Minus: Error
@@ -140,36 +137,44 @@ impl RedisEventListener for StandaloneEventListener {
     fn open(&mut self) -> Result<(), Error> {
         self.connect()?;
         self.auth()?;
+        let stream = self.stream.as_ref().unwrap();
+        let port = stream.local_addr()?.port().to_string();
+        let port = port.as_bytes();
+        self.send(b"REPLCONF", &[b"listening-port", port]);
+        self.response()?;
         Ok(())
     }
 
     fn close(&self) {
-        let option = self.writer.as_ref();
-        if self.writer.is_some() {
+        let option = self.stream.as_ref();
+        if self.stream.is_some() {
             println!("close connection with server...");
-            option.unwrap().get_ref().shutdown(Shutdown::Both).unwrap();
+            option.unwrap().shutdown(Shutdown::Both).unwrap();
         }
     }
 }
 
-pub fn new(host: &'static str, port: i32, password: &'static str) -> StandaloneEventListener {
+pub fn new(addr: SocketAddr, password: &'static str) -> StandaloneEventListener {
     StandaloneEventListener {
-        host,
-        port,
+        addr,
         password,
         config: config::default(),
-        writer: Option::None,
+        stream: Option::None,
     }
 }
 
 // 测试用例
 #[cfg(test)]
 mod tests {
+    use std::net::{IpAddr, SocketAddr};
+    use std::str::FromStr;
+
     use crate::{new, RedisEventListener};
 
     #[test]
     fn open() {
-        let mut redis_listener = new("localhost", 6379, "123");
+        let ip = IpAddr::from_str("127.0.0.1").unwrap();
+        let mut redis_listener = new(SocketAddr::new(ip, 6379), "123456");
         if let Err(error) = redis_listener.open() {
             panic!("couldn't connect to server: {}", error)
         }
