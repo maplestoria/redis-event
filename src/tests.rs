c5d1a9f7e2c4038941041e309ec0ad0fb80919b9
maplestoria <zkx6648@gmail.com>
2020-01-10 17:28:53 +0800

    添加测试用例

--- src/tests.rs
+++ src/tests.rs
@@ -110,11 +110,11 @@ mod test_cases {
     fn test_ziplist_that_compresses_easily() {
         let file = File::open("tests/rdb/ziplist_that_compresses_easily_1.rdb").expect("file not found");
         let mut file = io::from_file(file);
-    
+        
         struct TestRdbHandler {
             list: Vec<String>
         }
-    
+        
         impl RdbHandler for TestRdbHandler {
             fn handle(&mut self, data: Object) {
                 match data {
@@ -140,6 +140,50 @@ mod test_cases {
         }
         
         rdb::parse(&mut file, 0, &mut TestRdbHandler { list: Vec::new() }, &mut NoOpCommandHandler {})
+            .unwrap();
+    }
+    
+    #[test]
+    fn test_zset_ziplist() {
+        let file = File::open("tests/rdb/parser_filters.rdb").expect("file not found");
+        let mut file = io::from_file(file);
+        
+        struct TestRdbHandler {
+            map: HashMap<String, f64>
+        }
+        
+        impl RdbHandler for TestRdbHandler {
+            fn handle(&mut self, data: Object) {
+                match data {
+                    Object::SortedSet(set) => {
+                        let key = String::from_utf8_lossy(set.key);
+                        match key.as_ref() {
+                            "z1" => assert_eq!(2, set.items.len()),
+                            "z2" => assert_eq!(3, set.items.len()),
+                            "z3" => assert_eq!(2, set.items.len()),
+                            "z4" => assert_eq!(3, set.items.len()),
+                            _ => panic!("unknown key name: {}", key.as_ref())
+                        }
+                        for item in set.items {
+                            self.map.insert(String::from_utf8_lossy(&item.member).to_string(), item.score);
+                        }
+                    }
+                    Object::EOR => {
+                        assert_eq!(1.0, self.map.get("1").unwrap().clone());
+                        assert_eq!(2.0, self.map.get("2").unwrap().clone());
+                        assert_eq!(3.0, self.map.get("3").unwrap().clone());
+                        assert_eq!(10001.0, self.map.get("10002").unwrap().clone());
+                        assert_eq!(10003.0, self.map.get("10003").unwrap().clone());
+                        assert_eq!(1.0000000001E10, self.map.get("10000000001").unwrap().clone());
+                        assert_eq!(10000000002.0, self.map.get("10000000002").unwrap().clone());
+                        assert_eq!(1.0000000003E10, self.map.get("10000000003").unwrap().clone());
+                    }
+                    _ => {}
+                }
+            }
+        }
+        
+        rdb::parse(&mut file, 0, &mut TestRdbHandler { map: HashMap::new() }, &mut NoOpCommandHandler {})
             .unwrap();
     }
 }
